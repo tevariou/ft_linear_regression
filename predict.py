76d3626fd8e0655f096a7d5bbaef339c1b648f97
Teva RIOU <teva@yusofleet.com>
2020-03-24 20:39:13 +0100

    add model

---
+++ predict.py
@@ -0,0 +1,36 @@
+import model
+import csv
+import numpy as np
+
+
+def is_number(s):
+    if s is None:
+        return False
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+
+def read_theta():
+    with open('theta.csv') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            csv_file.close()
+            return float(row['theta1']), float(row['theta0'])
+
+
+def main():
+    model.train()
+    mileage = None
+    while is_number(mileage) is False:
+        mileage = input("What's your car mileage?\n")
+    theta1, theta0 = read_theta()
+    x, y, size = model.read_data()
+    mileage = (float(mileage) - np.amin(x)) / (np.amax(x) - np.amin(x))
+    estimated_price = model.denormalize(theta1 * mileage + theta0, np.amin(y), np.amax(y))
+    print("Estimated price = ", estimated_price)
+
+
+main()
