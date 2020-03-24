d789e47fc66f22a82267e492e3288bba9a286ada
Teva RIOU <teva@yusofleet.com>
2020-03-24 13:12:11 +0100

    plot

--- train.py
+++ train.py
@@ -4,24 +4,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def normalize(array):
-    a_min = np.amin(array)
-    a_max = np.amax(array)
-    return (array - a_min) / (a_max - a_min)
+def a_min_max(array):
+    return np.amin(array), np.amax(array)
 
 
-def denormalize(array, a_min, a_max):
-    return array * (a_max - a_min) + a_min
+def normalize_y(y):
+    a_min, a_max = a_min_max(y)
+    return (y - a_min) / (a_max - a_min)
 
 
 def normalize_x(x):
-    a_min = np.amin(x)
-    a_max = np.amax(x)
-    a_norm = (x - a_min) / (a_max - a_min)
-    size, _ = np.shape(a_norm)
+    a_min, a_max = a_min_max(x)
+    size, _ = np.shape(x)
     new_x = []
     for i in range(size):
-        new_x.append([a_norm[i, 0], 1])
+        new_x.append([(x[i, 0] - a_min) / (a_max - a_min), 1])
     return np.array(new_x)
 
 
@@ -36,37 +33,40 @@ def fetch_data():
                 if len(row) != 2 or row[0] != 'km' or row[1] != 'price':
                     sys.stderr.write("invalid data file\n")
                     return
-                size = 1
+                size += 1
                 continue
             if len(row) != 2:
                 continue
             y.append([float(row[1])])
             x.append([float(row[0])])
-    return np.array(x), np.array(y)
+            size += 1
+    return np.array(x), np.array(y), size
 
 
-def a_min_max(array):
-    return np.amin(array), np.amax(array)
+def denormalize(value, a_min, a_max):
+    return value * (a_max - a_min) + a_min
+
+
+def plot(theta, x, y, iteration):
+    plt.plot([0, denormalize(1, *a_min_max(x))],
+             [denormalize(theta[0, 0] * 0 + theta[1, 0], *a_min_max(y)), denormalize(theta[0, 0] * 1 + theta[1, 0], *a_min_max(y))],
+             label=f'iter = {iteration} theta1 = {"{0:.4f}".format(theta[0, 0])} theta0 = {"{0:.4f}".format(theta[1, 0])}')
 
 
 def main():
-    x, y = fetch_data()
-    x_min, x_max = a_min_max(x)
-    y_min, y_max = a_min_max(y)
-    new_x = normalize_x(x)
-    alter_x = normalize(x)
-    new_y = normalize(y)
+    x, y, size = fetch_data()
     theta = np.array([[0],
                      [0]])
-    alpha = 0.1
-    size, _ = np.shape(new_x)
-    for i in range(1000):
-        if i % 100 == 0:
-            plt.plot([0, 1], [theta[0, 0] * 0 + theta[1, 0], theta[0, 0] * 1 + theta[1, 0]])
-        grad = alpha / size * np.dot(new_x.T, np.dot(new_x, theta) - new_y)
-        theta = theta - grad
-    plt.plot([0, 1], [theta[0, 0] * 0 + theta[1, 0], theta[0, 0] * 1 + theta[1, 0]])
-    plt.scatter(alter_x, new_y)
+    alpha = 0.01
+    iterations = 10000
+    for i in range(iterations):
+        theta = theta - alpha / size * np.dot(normalize_x(x).T, np.dot(normalize_x(x), theta) - normalize_y(y))
+        if (i + 1) % 1000 == 0:
+            plot(theta, x, y, i)
+    plt.xlabel('km')
+    plt.ylabel('price')
+    plt.scatter(x, y)
+    plt.legend()
     plt.show()
 
 
